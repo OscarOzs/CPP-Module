ef7332f0448537442a9bff1971a27c8eeec622c0
Oscar OZSERTTAS <oozsertt@student.42.fr>
2022-05-16 17:32:41 +0200

    visibility private and public example added

--- examples/visibility/Sample_class.hpp
+++ examples/visibility/Sample_class.hpp
@@ -10,13 +10,14 @@ public :
 	Sample(void);
 	~Sample(void);
 
-	void	publicFoo(void) const;
+	void	publicBar(void) const;
 
 private:
 
 	int	_privateFoo;
 
-	void	_privrateBar(void) const;
-};
+	void	_privateBar(void) const;
+}; // l'interet de fonction, variables "private" est de pouvoir les utiliser seulement dans cette classe.
+// coupler a une fonction avec initialization list, elle protege la valeur des privates. (if valeur < 0 ), if (valeur > 1000)
 
 #endif
