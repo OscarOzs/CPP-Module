89a5aae20ef57050d37466f1f3181d884e57a63b
Oscar OZSERTTAS <oozsertt@student.42.fr>
2022-05-17 20:30:33 +0200

    makefile done but need adjustment, megaphone not finished yet

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,35 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 02:27:58 by oozsertt          #+#    #+#             */
-/*   Updated: 2022/05/13 02:53:07 by oozsertt         ###   ########.fr       */
+/*   Updated: 2022/05/17 20:28:52 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <iostream>
+
+int main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac >= 2)
+	{
+		i = 0;
+		while (av[i])
+		{
+			j = 0;
+			while (av[i][j] != '\0')
+			{
+				if (isupper(av[i][j]) == 0)
+					std::cout << toupper(av[i][j]);
+				else
+					std::cout << av[i][j];
+				j++;
+			}
+			i++;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+}
