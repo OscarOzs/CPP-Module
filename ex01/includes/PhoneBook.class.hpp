3841d58539ba6ddff8922e9158a07268904083df
Oscar OZSERTTAS <oozsertt@student.42.fr>
2022-05-19 17:14:55 +0200

    phonebook (ex01) started

---
+++ ex01/includes/PhoneBook.class.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 15:49:28 by oozsertt          #+#    #+#             */
+/*   Updated: 2022/05/19 15:56:33 by oozsertt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_H
+# define PHONEBOOK_CLASS_H
+
+# include <iostream>
+# include <string>
+
+class PhoneBook
+{
+public:
+
+	PhoneBook(void);
+	~PhoneBook(void);
+};
+
+#endif
---
+++ ex01/srcs/PhoneBook.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 14:13:48 by oozsertt          #+#    #+#             */
+/*   Updated: 2022/05/19 16:14:23 by oozsertt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "PhoneBook.class.hpp"
+
+void	StartPhoneBook(void)
+{
+	// PhoneBook MyPhoneBook;
+
+	std::string command;
+	std::cin >> command;
+	std::cout << command;
+}
+
+int main(void)
+{
+	StartPhoneBook();
+}
