9d5f2714addeff852cd9b18af3ca68f01392f448
Ozserttas Oscar <ozserttas.oscar@gmail.com>
2022-09-16 13:59:38 +0200

    save

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: oozsertt <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 02:27:58 by oozsertt          #+#    #+#             */
-/*   Updated: 2022/09/16 13:34:23 by oozsertt         ###   ########.fr       */
+/*   Updated: 2022/09/16 13:51:56 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 int main(int ac, char **av)
 {
+	std::string str;
+
 	if (ac >= 2)
 	{
 		for (int i = 1; i < ac; i++)
 		{
-			std::string str (av[i]);
-			for (int j = 0; av[i][j] != '\0'; j++)
-				std::cout << (char)std::toupper(av[i][j]);
+			str.assign(av[i]);
+			for (int j = 0; str[j] != '\0'; j++)
+				std::cout << (char)std::toupper(str[j]);
 		}
 	}
 	else
