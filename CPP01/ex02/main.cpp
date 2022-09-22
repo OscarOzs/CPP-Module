e0db9218d78d9b420f5f0d9808ef07b422dcf9db
Ozserttas Oscar <ozserttas.oscar@gmail.com>
2022-09-22 09:52:10 +0200

    Done

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 09:00:00 by oozsertt          #+#    #+#             */
+/*   Updated: 2022/09/22 09:51:30 by oozsertt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string		str = "HI THIS IS BRAIN";
+	std::string		*stringPTR = &str;
+	std::string&	stringREF = str;
+
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
