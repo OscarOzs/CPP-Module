e808fef79cf3c0edfb2fa974d73a003ff99d3a73
Ozserttas Oscar <ozserttas.oscar@gmail.com>
2022-10-04 11:21:46 +0200

    ex00 Done

---
+++ CPP03/ex00/main.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 17:08:01 by oozsertt          #+#    #+#             */
+/*   Updated: 2022/10/04 11:19:40 by oozsertt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+void	JohnExhausted(void)
+{
+	ClapTrap	John;
+
+	ClapTrap	Sam("John");
+	John = Sam;
+	for (int i = 0; i < 11; i++)
+	{
+		John.attack("Enemy");
+	}
+	John.beRepaired(5);
+}
+
+void	RicoDies(void)
+{
+	ClapTrap	Rico("Rico");
+
+	ClapTrap	random(Rico);
+	for (int i = 0; i < 11; i++)
+	{
+		random.takeDamage(1);
+	}
+	random.attack("Monster");
+	random.beRepaired(10);
+}
+
+void	OscarRepair(void)
+{
+	ClapTrap	random("Oscar");
+
+	for (int i = 0; i < 5; i++)
+	{
+		random.beRepaired(15);
+	}
+	random.takeDamage(5);
+}
+
+int main(void)
+{
+	// JohnExhausted();
+	// RicoDies();
+	// OscarRepair();
+	return(0);
+}
