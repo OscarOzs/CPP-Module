ee721034088b560b5d08ddd9f2e1fb8fd5e46dd4
Ozserttas Oscar <ozserttas.oscar@gmail.com>
2022-10-25 11:32:16 +0200

    copy and assignation of dog and cat now deeply copy

--- CPP04/ex01/Dog.cpp
+++ CPP04/ex01/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 11:09:58 by oozsertt          #+#    #+#             */
-/*   Updated: 2022/10/13 17:51:55 by oozsertt         ###   ########.fr       */
+/*   Updated: 2022/10/25 11:31:03 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ Dog::Dog(Dog & src) : AAnimal(src)
 	this->_brain = new Brain(*(src._brain));
 	std::cout << "Dog copy constructor called" << std::endl;
 	this->type = src.type;
+	for (int i = 0; i < 100; i++)
+		this->_brain->ideas[i] = src._brain->ideas[i];
 	return;
 }
 
@@ -40,6 +42,8 @@ Dog & Dog::operator=(Dog & rhs)
 	this->type = rhs.type;
 	delete this->_brain;
 	this->_brain = new Brain(*(rhs._brain));
+	for (int i = 0; i < 100; i++)
+		this->_brain->ideas[i] = rhs._brain->ideas[i];
 	std::cout << "Dog asignation operator called" << std::endl;
 	return (*this);
 }
