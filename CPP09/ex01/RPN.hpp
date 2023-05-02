2aed66cfad9a2f56c4f165bd10817afbfb6d9254
Oscar OZSERTTAS <oozsertt@student.42.fr>
2023-05-02 23:29:29 +0200

    define used to debug removed

--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -6,14 +6,12 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 17:11:23 by oozsertt          #+#    #+#             */
-/*   Updated: 2023/04/25 20:40:34 by oozsertt         ###   ########.fr       */
+/*   Updated: 2023/05/02 23:28:45 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RPN_HPP
 # define RPN_HPP
-
-# define ICI std::cout << "ICI" << std::endl;
 
 # include <iostream>
 # include <sstream>
